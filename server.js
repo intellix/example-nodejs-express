9b9c51300945592bf8e3ecb0a973f78b03e38d2e
Dominic Watson <intellix@users.noreply.github.com>
2017-03-06 19:16:09 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.get('/', function(req, res){
   var release = process.env.WORKFLOW_RELEASE;
 
   if (typeof(message) == "undefined") {
-  	poweredBy = "Deis";
+  	poweredBy = "Deis + Craig daaaavid";
   }
 
   exec('hostname', function(error, stdout, stderr) {
